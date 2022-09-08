d01848c8ad2514fb9515486896f97787cd72b825
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2022-09-08 17:10:37 -0300

    feat(Configurações iniciais): Configura o Dockerfile e o ESlint

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// teste
+
+function App() {
+  return (
+    <div>
+      <p>My Portfolio</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-portfolio/src/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const root = createRoot(
+  document.getElementById('root'),
+);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();

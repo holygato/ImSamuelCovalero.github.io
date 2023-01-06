83283d6f4dbce1466286f997cb3b035cdfea2c97
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2023-01-06 11:42:20 -0300

    improvement(Tecnologias): Adiciona SOLID e Mongoose em tecnologias aprendidas

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -28,7 +28,9 @@ function MySkills() {
     { nome: 'Chai', imagem: 'https://img.shields.io/badge/Chai-A30701?style=for-the-badge&logo=chai&logoColor=white' },
     { nome: 'TypeScript', imagem: 'https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white' },
     { nome: 'Object-Oriented Programming', imagem: 'https://img.shields.io/badge/OOP-000000?style=for-the-badge&logo=oop&logoColor=white' },
+    { nome: 'SOLID', imagem: 'https://img.shields.io/badge/SOLID-000000?style=for-the-badge&logo=solid&logoColor=white' },
     { nome: 'MongoDB', imagem: 'https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white' },
+    { nome: 'Mongoose', imagem: 'https://img.shields.io/badge/Mongoose-47A248?style=for-the-badge&logo=mongoose&logoColor=white' },
   ];
 
   const tecnologiesLearning = [

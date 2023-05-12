8f2885d5f19d033b0d547e79300601e34c275fcd
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2023-05-12 20:39:43 -0300

    improvement(Locales): Cria a pasta que será responsável por armazenar os textos de cada idioma.

---
+++ src/Locales/ptBR.jsx
@@ -0,0 +1,46 @@
+// arquivo responsável por conter a estrutura textual do site, em português do Brasil.
+const ptBR = {
+  principal: {
+    principal1: {
+      firstPart: 'Olá, eu sou Samuel dos Reis,',
+      secondPart: 'seja muito bem vindo (a) ao meu Portfólio!',
+      thirdPart: 'Aqui você conhecerá um pouco sobre mim e aonde me encontrar, as tecnologias que conheço e utilizo e terá acesso a alguns projetos que desenvolvi.',
+      contactMe: 'Me Contacte!',
+      slideHint: 'Deslize',
+    },
+    principal2: {
+      moreAboutMe: 'Saiba mais sobre mim...',
+      aboutMe: 'Sobre mim',
+      curriculum: 'Currículum',
+      mySkills: 'Minhas Skills',
+    },
+    principal3: {
+      projects: 'Projetos',
+    },
+    principal4: {
+      contactMe: 'Me Contacte!',
+    },
+    footer: {
+      phrase: 'Transformação pela informação, evolução pela tecnologia',
+    },
+  },
+
+  sobre: {
+    header: 'Sobre Mim:',
+    introduction: 'Olá, meu nome é Samuel Covalero dos Reis, resido em São Carlos, SP. Sou desenvolvedor Full Stack, formado em Engenharia da Computação e completei o curso de Desenvolvimento Web na Trybe.',
+    passion: 'Sempre me interessei por cultura Geek, inovação e tecnologia, e me fascina a possibilidade de impactar positivamente a vida das pessoas através da mesma, transformando o mundo em um lugar mais próspero, conectado e justo.',
+    belief: 'Eu acredito no pensamento surpreendente, no improvável e na melhora da realidade. E a forma com que contribuo para isto é através da criatividade, do foco, da paixão em codar, da constância e da busca pela excelência em meus códigos.',
+    curriculum: 'Currículum',
+    mySkills: {
+      title: 'Linguagens e Tecnologias:',
+      know: 'Conheço:',
+      studying: 'Estou estudando:',
+      willLearn: 'Pretendo aprender:',
+    },
+    footer: {
+      phrase: 'Seja a mudança que quer ver no mundo, tudo muda quando a gente muda',
+    },
+  },
+};
+
+export default ptBR;
